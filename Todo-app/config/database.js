a8ed3829b802258789469c916e3ec7d7e6d4f8bf
pdetender <peter@pdtit.be>
2018-11-22 11:06:05 -0600

    changedconnectionstring
    changed connection string

--- Todo-app/config/database.js
+++ Todo-app/config/database.js
@@ -1,5 +1,5 @@
 module.exports = {
-    remoteUrl : 'mongodb://todosazure:v1ca8pp8s1weqNLNUJJW2mnrJLk5N7xKIOr0p9ATYvonprP4b88Oj128WR1wd41oNhoxIon9hrl5EXbFXjCMUA==@todosazure.documents.azure.com:10255/?ssl=true&replicaSet=globaldb',
+    remoteUrl : 'mongodb://todosazure:<yourconnectionstringhere>',
     
     localUrl: 'mongodb://localhost/meanstacktutorials'
 };
